8ba6760f495db415412a164b162fe725f2495d1e
Ladgelson <micaiasladgelsondasilva@gmail.com>
2020-05-08 16:51:32 -0300

    Funçao busca e like

--- scripts/pagination.js
+++ scripts/pagination.js
@@ -1,147 +1,195 @@
-const items = [
+const allItems = [
   {
     "photo": "https://a0.muscache.com/im/pictures/e6c4b347-49c7-4840-8c00-df36a2a273da.jpg?aki_policy=x_large",
     "property_type": "Apartamento",
     "name": "Apartment in Son Parc, wonderful views",
-    "price": 433
+    "price": 433,
+    "localidade": "Fortim",
+    "liked": false
   },
   {
     "photo": "https://a0.muscache.com/im/pictures/4a5326cb-95e4-4220-a4d8-c91f50cf784c.jpg?aki_policy=xx_large",
     "property_type": "Apartamento",
     "name": "APARTAMENTO IDEAL PAREJAS EN SON PARC",
-    "price": 368
+    "price": 368,
+    "localidade": "Fortim",
+    "liked": false
   },
   {
     "photo": "https://a0.muscache.com/im/pictures/77a102a4-cf65-475e-be60-4d592307ab4a.jpg?aki_policy=xx_large",
     "property_type": "Casa",
     "name": "Casa Charmosa Bem Localizada",
-    "price": 70
+    "price": 70,
+    "localidade": "Fortim",
+    "liked": false
   },
   {
     "photo": "https://a0.muscache.com/im/pictures/83de85c5-ce24-4cc4-ae85-dfb2300b4a06.jpg?aki_policy=xx_large",
     "property_type": "Casa",
     "name": "Quartos pertinho do Anhembi!",
-    "price": 189
+    "price": 189,
+    "localidade": "Fortim",
+    "liked": false
   },
   {
     "photo": "https://a0.muscache.com/im/pictures/d5dbb5b2-12a3-4de5-87d0-5a66841eb731.jpg?aki_policy=xx_large",
     "property_type": "Chácara",
     "name": "Charming Chalet, private pool, free AC & WiFi",
-    "price": 737
+    "price": 737,
+    "localidade": "Fortim",
+    "liked": false
   },
   {
     "photo": "https://a0.muscache.com/im/pictures/ce7dd48a-8125-4cea-a1b7-27ce0068d5d8.jpg?aki_policy=x_large",
     "property_type": "Chácara",
     "name": "Menorca Green Park C7",
-    "price": 520
+    "price": 520,
+    "localidade": "Fortim",
+    "liked": false
   },
   {
     "photo": "https://a0.muscache.com/im/pictures/bea86611-2f82-487a-a62e-17ab268515de.jpg?aki_policy=xx_large",
     "property_type": "Chácara",
     "name": "Villa Monty",
-    "price": 1291
+    "price": 1291,
+    "localidade": "Fortim",
+    "liked": false
   },
   {
     "photo": "https://a0.muscache.com/im/pictures/4cfeba0b-a0c3-4ae4-87c9-5e9349608814.jpg?aki_policy=xx_large",
     "property_type": "Estúdio",
     "name": "Bonito penthouse cerca del aeropuerto CDMX",
-    "price": 73
+    "price": 73,
+    "localidade": "Fortim",
+    "liked": false
   },
   {
     "photo": "https://a0.muscache.com/im/pictures/ac081c48-0161-4cde-a0e6-77b3bd933f94.jpg?aki_policy=xx_large",
     "property_type": "Estúdio",
     "name": "The Backpacker's Retreat - Balcony",
-    "price": 51
+    "price": 51,
+    "localidade": "Fortim",
+    "liked": false
   },
   {
     "photo": "https://a0.muscache.com/im/pictures/dc72255f-7965-4964-b89e-7bfb9f4aa939.jpg?aki_policy=x_large",
     "property_type": "Estúdio",
     "name": "10min airport, Foro Sol, Palacio de los Deportes",
-    "price": 81
+    "price": 81,
+    "localidade": "Fortim",
+    "liked": false
   },
   {
     "photo": "https://a0.muscache.com/im/pictures/60f11b07-3aa2-4e03-ac03-6a0c12fbc35b.jpg?aki_policy=x_large",
     "property_type": "Estúdio",
     "name": "Acogedor depto cerca aeropuerto, foro Sol, Centro",
-    "price": 77
+    "price": 77,
+    "localidade": "Fortim",
+    "liked": false
   },
   {
     "photo": "https://a0.muscache.com/im/pictures/f2379eb0-d77e-4a99-9df5-f16032693f0d.jpg?aki_policy=xx_large",
     "property_type": "Loft",
     "name": "Sol Nascente da Prainha",
-    "price": 200
+    "price": 200,
+    "localidade": "Fortim",
+    "liked": false
   },
   {
     "photo": "https://a0.muscache.com/im/pictures/5ed611f3-fb3f-489e-b1cb-d5f26ea80c4c.jpg?aki_policy=xx_large",
     "property_type": "Loft",
     "name": "Ap novo com wi-fi na Prainha - Arraial do Cabo",
-    "price": 145
+    "price": 145,
+    "localidade": "Fortim",
+    "liked": false
   },
   {
     "photo": "https://a0.muscache.com/im/pictures/207ef44d-fbe6-4fcd-9824-91b65de4b3cb.jpg?aki_policy=xx_large",
     "property_type": "Quarto",
     "name": "Suite @ Subway Trianon Masp",
-    "price": 69
+    "price": 69,
+    "localidade": "Fortim",
+    "liked": false
   },
   {
     "photo": "https://a0.muscache.com/im/pictures/f94985db-d4db-4c10-8b9b-82342746601d.jpg?aki_policy=x_large",
     "property_type": "Quarto",
     "name": "Quarto Arouche Centro SP Ótima Localização",
-    "price": 45
+    "price": 45,
+    "localidade": "Fortim",
+    "liked": false
   },
   {
     "photo": "https://a0.muscache.com/im/pictures/8b397c47-01af-4476-9fb8-784ab4df2c91.jpg?aki_policy=xx_large",
     "property_type": "Quarto",
     "name": "Quarto 1 - Parque da Aclimacão",
-    "price": 55
+    "price": 55,
+    "localidade": "Fortim",
+    "liked": false
   },
   {
     "photo": "https://a0.muscache.com/im/pictures/c3c6432f-b140-4768-8d61-c160e639b7a8.jpg?aki_policy=x_large",
     "property_type": "Quarto",
     "name": "Existe amor em SP",
-    "price": 55
+    "price": 55,
+    "localidade": "Fortim",
+    "liked": false
   },
   {
     "photo": "https://a0.muscache.com/im/pictures/306e0369-ab79-40be-bdb7-c87340dcdbc1.jpg?aki_policy=xx_large",
     "property_type": "Quarto",
     "name": "Confortável quarto de casal px. à Av. Paulista",
-    "price": 80
+    "price": 80,
+    "localidade": "Fortim",
+    "liked": false
   },
   {
     "photo": "https://a0.muscache.com/im/pictures/f5abd356-8a66-495b-b00b-877e7eae0323.jpg?aki_policy=xx_large",
     "property_type": "Quarto",
     "name": "Room Higienopolis São Paulo",
-    "price": 80
+    "price": 80,
+    "localidade": "Fortim",
+    "liked": false
   },
   {
     "photo": "https://a0.muscache.com/im/pictures/f1899868-21d0-44c2-b253-8b927821e178.jpg?aki_policy=xx_large",
     "property_type": "Quarto",
     "name": "Confortável quarto solteiro px. à Av. Paulista",
-    "price": 80
+    "price": 80,
+    "localidade": "Fortim",
+    "liked": false
   },
   {
     "photo": "https://a0.muscache.com/im/pictures/6b46fa03-8ef3-4370-8158-390935279dcf.jpg?aki_policy=xx_large",
     "property_type": "Quarto",
     "name": "Modern, Cozy + all you need! Muito aconchegante!!",
-    "price": 66
+    "price": 66,
+    "localidade": "Fortim",
+    "liked": false
   },
   {
     "photo": "https://a0.muscache.com/im/pictures/f074bead-ee2d-4c91-a958-2a0360e1ad7c.jpg?aki_policy=xx_large",
     "property_type": "Sítio",
     "name": "Recanto Rosana - Quartos e estrutura completa",
-    "price": 39
+    "price": 39,
+    "localidade": "Fortim",
+    "liked": false
   },
   {
     "photo": "https://a0.muscache.com/im/pictures/de890df5-1279-481f-989c-97847f8482cd.jpg?aki_policy=xx_large",
     "property_type": "Sítio",
     "name": "Suite na Montanha - Refúgio do Artista",
-    "price": 77
+    "price": 77,
+    "localidade": "Fortim",
+    "liked": false
   },
   {
     "photo": "https://a0.muscache.com/im/pictures/17b61b71-3b06-4cec-bf8f-f32daea24f39.jpg?aki_policy=xx_large",
     "property_type": "Sítio",
     "name": "Sítio Costa do Sol",
-    "price": 100
+    "price": 100,
+    "localidade": "Fortim",
+    "liked": false
   }
 ]
 
@@ -149,11 +197,11 @@ const listElements = document.getElementById('items')
 const pagination = document.getElementById('pagination')
 
 let currentPage = 1;
-let pageOut;
 let row = 5;
+let items = [...allItems]
+
 
 function displayList(items, wrapper, rowsPerPage, page) {
-  console.log(rowsPerPage, page)
   wrapper.innerHTML = "";
   page--;
 
@@ -162,63 +210,106 @@ function displayList(items, wrapper, rowsPerPage, page) {
   let paginatedItems = items.slice(start, end)
   for (let i = 0; i < paginatedItems.length; ++i) {
     let item = paginatedItems[i]
-    wrapper.innerHTML += 
-    `<div class="row">
+    wrapper.innerHTML +=
+      `<div class="row">
         <div class="col-md-7">
-            <div class="acomodacao">
-                <img class="img-card" src=` + item.photo + `>
-                <div class="info">
-                    <div class="title-and-icon">
-                        <div class="card-title">
-                            <strong>` + item.name + `</strong>
-                        </div>
-                        <div class="align-heart">
-                            <img class="love-icon" src="img/love-and-romance.png">
-                        </div>
-                    </div>
-                    <div><span>` + item.property_type + `</span></div>
-                    <div class="value">
-                        <span> <strong>R$` + item.price + `</strong>/noite</span>
-                    </div>
+          <div class="acomodacao">
+            <img class="img-card" src=` + item.photo + `>
+            <div class="info">
+              <div class="title-and-icon">
+                <div class="card-title">
+                  <strong>` + item.name + `</strong>
                 </div>
+                <div class="align-heart" onClick="like(`+ (5*page + i) +`)">
+                  <img class="love-icon" 
+                      src=` + selectIcon(item.liked) + `
+                      id=`+ (5*page + i) + `
+                      >
+                </div>
+              </div>
+              <div><span>` + item.localidade + ", " + item.property_type + `</span></div>
+              <div class="value">
+                <span> <strong>R$` + item.price + `</strong>/noite</span>
+              </div>
             </div>
+          </div>
         </div>
-    </div>`
+      </div>`
   }
 }
 
-function setupPagination(items, wrapper, rowsPerPage){
+function setupPagination(items, wrapper, rowsPerPage) {
   wrapper.innerHTML = ""
 
   let pageCount = Math.ceil(items.length / rowsPerPage)
-  for(let i = 1; i < pageCount + 1; ++i){
+  for (let i = 1; i < pageCount + 1; ++i) {
     let button = paginationButton(i, items)
     wrapper.appendChild(button)
   }
-  console.log(wrapper)
 }
 
-function paginationButton(page, items){
+
+function paginationButton(page, items) {
   let button = document.createElement('button')
   button.innerText = page
-  if(currentPage == page) button.classList.add('active')
-  button.addEventListener('click', ()=> {
+  if (currentPage == page) button.classList.add('active')
+  button.addEventListener('click', () => {
     currentPage = page
     displayList(items, listElements, row, currentPage)
 
     let currentButton = document.querySelector('#pagination button.active');
-		currentButton.classList.remove('active');
+    currentButton.classList.remove('active');
 
-		button.classList.add('active');
+    button.classList.add('active');
   })
 
   return button
 }
 
-function changePage(){
+function changePage() {
   currentPage = pageOut
   displayList(items, listElements, row, currentPage)
 }
 
 displayList(items, listElements, row, currentPage)
 setupPagination(items, pagination, row)
+
+function buscarLocalizacao() {
+  let value = document.getElementById('localizacao').value
+  console.log(value)
+  value.toLowerCase()
+  items = items.filter((item) => {
+    return item.localidade.toLowerCase() == value
+  })
+  console.log(items)
+  currentPage = 1
+  displayList(items, listElements, row, currentPage)
+  setupPagination(items, pagination, row)
+}
+
+function selectIcon(isLiked) {
+  if (isLiked) return `"img/love-and-romance-fill.png"`
+  return `"img/love-and-romance.png"`
+}
+
+function like(id){
+  let image = document.getElementById(id)
+  let item = items[id]
+  item.liked = !item.liked
+  image.src = selectIcon(item.liked)
+  displayList(items, listElements, row, currentPage)
+}
+
+function listFavorites(){
+  items = items.filter((item)=> {return item.liked})
+  currentPage = 1
+  displayList(items, listElements, row, currentPage)
+  setupPagination(items, pagination, row)
+}
+
+function reset() {
+  items = [...allItems]
+  currentPage = 1
+  displayList(items, listElements, row, currentPage)
+  setupPagination(items, pagination, row)
+}
